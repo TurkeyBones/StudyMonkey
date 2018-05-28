16dbf40147365f9aa5b8695132f0685d9b99cdc7
Joshua Hardy <jshardy@outlook.com>
2018-05-28 15:10:54 -0700

    Added questions menu

--- src/Category.kt
+++ src/Category.kt
@@ -1,20 +1,49 @@
 import java.io.Serializable
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.Random
 
+data class FlashCard(val flashcard: String, val answer: String, var last_good_answer: LocalDate = LocalDate.now(), var last_answered_correctly: Boolean = true) : Serializable {
+    fun try_question(possible_answer: String) : Boolean {
+        if(possible_answer == answer) {
+            last_good_answer = LocalDate.now()
+            last_answered_correctly = true
+        }
+        else {
+            last_answered_correctly = false
+        }
+        return last_answered_correctly
+    }
+}
 data class Category(val category: String) : Serializable {
     var categoryName: String = category
-    private var flashCardList = ArrayList<String>()
+    var flashCardList = ArrayList<FlashCard>()
+    private var current_flash_card = 0
+    private val random = Random()
 
-    fun addFlashCard(flashcard: String) {
-        flashCardList.add(flashcard);
+    fun addFlashCard(flashcard: String, answer: String) {
+        flashCardList.add(FlashCard(flashcard, answer))
     }
 
     fun removeFlashCard(flashcard: String) {
-        flashCardList.remove(flashcard);
+        flashCardList.forEach({
+            if(it.flashcard == flashcard) {
+                flashCardList.remove(it)
+            }
+        })
     }
 
     //this overrides the array index operator
-    fun get(index: Int) : String
-    {
+    fun get(index: Int) : FlashCard {
         return flashCardList[index]
+    }
+
+    fun size() : Int {
+        return flashCardList.size
+    }
+
+    fun rand(from: Int = 0, to: Int = size()) : Int {
+        return random.nextInt(to - from) + from
     }
 }
--- src/Main.kt
+++ src/Main.kt
@@ -1,6 +1,5 @@
 import java.io.*
 import Category
-import sun.plugin2.liveconnect.ArgumentHelper.writeObject
 
 
 
@@ -9,32 +8,34 @@ var categories = ArrayList<Category>()
 
 fun main(args: Array<String>)
 {
-//    var c = Category("CST 238")
+//    var c = Category("CST 101")
 //
-//    c.addFlashCard("C++ is a ______ language?")
-//    c.addFlashCard("C is a _______ language?")
-//    c.addFlashCard("Delphi is based on ______ language?")
+//    c.addFlashCard("C++ is a ______ language?", "object-oriented")
+//    c.addFlashCard("C is a _______ language?", "procedural language")
+//    c.addFlashCard("Delphi is based on ______ language?", "Pascal/Algol")
 //    categories.add(c)
 //
 //    c = Category("CST 116")
-//    c.addFlashCard("What is the main entry point of a C++ program?");
-//    c.addFlashCard("What is an alternative to the IF/ELSE statement?");
-//    c.addFlashCard("What is return used for?");
+//    c.addFlashCard("What is the main entry point of a C++ program?", "int main()");
+//    c.addFlashCard("What is an alternative to the IF/ELSE statement?", "switch()");
+//    c.addFlashCard("What is return used for?", "return from function");
 //    categories.add(c)
 //
 //    c = Category("CST 136")
-//    c.addFlashCard("What can't C do?")
-//    c.addFlashCard("What can C++ do?")
-//    c.addFlashCard("Who can do it?")
+//    c.addFlashCard("What can't C do?", "C is God")
+//    c.addFlashCard("What can C++ do?", "OOP")
+//    c.addFlashCard("Who can do it?", "Bjarne Stroustrup")
 //    categories.add(c)
 
-    var answer = displayMenu(mainMenuString, '1', '4')
 
     //write_category_file("/Users/jshardy/OneDrive/Documents/Projects/StudyMonkey/src/categories.dat")
     open_category_file("/Users/jshardy/OneDrive/Documents/Projects/StudyMonkey/src/categories.dat")
+
+    var answer = displayMenu(mainMenuString, '1', '4')
+
     when (answer) {
         1 -> displaySelectCategory()
-        2 -> print("x == 2")
+        2 -> print("x == 2")    //edit
         else -> { // Note the block
             print("x is neither 1 nor 2")
         }
@@ -48,21 +49,61 @@ fun displayMenu(menu: String, startIndex: Char, endIndex: Char) : Int
         print(menu)
         print(":")
         answer = readLine()
-    } while(answer == null || answer[0] < startIndex || answer[0] > endIndex)
+
+        if(answer != null)
+            answer.trim()
+
+1    } while(answer == null || answer[0].toChar() < startIndex || answer[0].toChar() > endIndex)
 
     return answer.toInt()
 }
 
 fun displaySelectCategory()
 {
-    var i = 1
+    var i: Int = 0
     var newString = ""
 
     categories.forEach({
-        newString += i.toString() + " - " + it.categoryName + "\n"
         i++
+        newString += i.toString() + " - " + it.categoryName + "\n"
     })
-    displayMenu(newString.toString(), '1', i.toChar())
+
+    val x = displayMenu(newString, '1', '3')
+    gotocategory(categories[x])
+}
+
+fun gotocategory(category: Category)
+{
+    for(flashcard in category.flashCardList)
+    {
+        var newString = "\n" + flashcard.flashcard + "\n"
+        var inserted = false
+        var answer_array = ArrayList<String>()
+
+        for (i in 1..3)
+        {
+            var rindex = category.rand()
+            var whereAnswer = category.rand(0, 1)
+
+            if(whereAnswer == 0 || inserted) {
+                answer_array.add(category.flashCardList[rindex].answer)
+                newString += i.toString() + ". " + category.flashCardList[rindex].answer + "\n"
+            }
+            else if(!inserted) {
+                answer_array.add(flashcard.answer)
+                newString += i.toString() + ". " + flashcard.answer + "\n"
+                inserted = true
+            }
+        }
+        var answer_index = displayMenu(newString, '1', '3')
+        //remember to subtract one
+        var correct_answer = flashcard.try_question(answer_array[answer_index - 1])
+
+        if(correct_answer)
+            println("Correct!!!")
+        else
+            println("Incorrect...")
+    }
 }
 
 fun write_category_file(file: String)
